446405f7721362f5d9d311b0a75309539a590ee0
raj <raj.kumar@subcodevs.com>
2020-07-15 12:53:42 +0530

    chnages

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ io.on("connection", function(socket) {
   Usercounter = Usercounter + 1;
   socket.broadcast.emit("user", Usercounter);
   console.log("a user is connected");
+
+  socket.on('join', function (data) {
+    socket.join(data.user); // We are using room of socket io
+  });
   
   socket.on("disconnect", function() {
     Usercounter = Usercounter - 1;
@@ -23,11 +27,12 @@ io.on("connection", function(socket) {
   socket.on("audioMessage", function(users) {
     console.log(users);
     let message  = users.message;
-    users.to.forEach(element => {
+    io.sockets.in(users.to[0]).emit("audioMessage", message);
+    /*users.to.forEach(element => {
       console.log(element);
       io.to(element).emit("audioMessage", message);
       //socket.broadcast.to(element).emit("audioMessage", message);
-    });
+    });*/
    
   });
 });
