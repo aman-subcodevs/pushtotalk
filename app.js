d4c88c034c407f06e3ac30b941ab1307184bc068
Aman Gupta <aman@subcodevs.com>
2020-07-21 18:05:18 +0530

    socket fixes, removed room dependency for indivdual audio transfers

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// const { disconnect } = require("process");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const compression = require("compression");
-const { ObjectId } = require("mongodb")
 const app = require("express")();
 const http = require("http").Server(app);
-const clinet = require('mongodb').MongoClient;
+const mongoClient = require('mongodb').MongoClient;
 const oneSignal = require('onesignal-node');
 const cors = require('cors')
 dotenv.config()
@@ -17,25 +15,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let usersCollection = null;
 const url = process.env.MONGO_URI
-clinet.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+mongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.log('Error connecting the database', err);
+  }
   const db = client.db("pushtotalk");
   usersCollection = db.collection("users")
-  console.log("Connected correctly to server");
+  console.log("MongoDB connection established");
 });
 
 let io = require("socket.io")(http);
-const client = new oneSignal.Client(process.env.OS_APP_ID, process.env.OS_APP_KEY);
+const oneSignalClient = new oneSignal.Client(process.env.OS_APP_ID, process.env.OS_APP_KEY);
 
-io.attach(http, {
-  pingInterval: 10000,
-  pingTimeout: 5000,
-  cookie: false
-});
+io.attach(http, { pingInterval: 10000, pingTimeout: 5000, cookie: false });
 
-let Usercounter = 0;
-sockets = [];
-people = {};
-disconnectUsers = {};
+
+let totalUserCount = 0;
+let people = {};
 
 app.get("/", function (req, res) {
   res.send({ data: "success" });
@@ -67,7 +63,7 @@ const sendPushNotification = async (users, audioUrl = "https://file-examples-com
   };
 
   try {
-    const resp = await client.createNotification(notification)
+    const resp = await oneSignalClient.createNotification(notification)
     console.log('Push notifications sent succssfully');
   } catch (err) {
     console.log('Push notification err', err);
@@ -84,77 +80,56 @@ app.post("/login-user", async (req, res) => {
   }
 })
 
+io.use(function (socket, next) {
+  sockets.push(socket.id);
+  people[socket.id] = {};
+  totalUserCount++;
+  socket.broadcast.emit("user", totalUserCount);
+	return next();
+});
+
 io.on("connection", function (socket) {
-  Usercounter = Usercounter + 1;
-  socket.broadcast.emit("user", Usercounter);
-  // console.log("a user is connected");
-
-
   socket.on('join', function (data) {
-    console.log('join data', data);
-    sockets.push(socket);
     data.user.forEach(element => {
       people[socket.id] = { element: element };
     })
-    console.log('people', people);
-  });
-
-  socket.on("minimized", function (data) {
-    console.log(data)
-  });
-
-  socket.on('ping', function () {
-    console.log('ping');
-    socket.emit('pong');
   });
 
   socket.on("disconnect", function () {
-    console.log('user disconnected', 'socket.id', socket.id)
+    console.log('user disconnected', people[socket.id]);
     delete people[socket.id];
-    sockets.splice(sockets.indexOf(socket), 1);
-    console.log("user disconnected");
-    // socket.emit('rejoin');
   });
 
   socket.on("audioMessage", async function (data) {
     let message = data.message;
-    console.log('is group', data.group)
     if (!data.group) {
+      // individual audio msg
       let receiverSocketId = findUserById(data.to[0]);
-      console.log('receiverSocketId', receiverSocketId)
+
       if (receiverSocketId) {
         let receiver = people[receiverSocketId];
         console.log('receiver', receiver)
-        let room = getARoom(people[socket.id], receiver);
-        if (io.sockets.connected[receiverSocketId]) {
-          console.log('connected')
-          io.sockets.connected[receiverSocketId].join(room);
-          io.sockets.in(room).emit("audioMessage", message);
+
+        // check if receiver and sender are connected
+        if (io.sockets.connected[receiverSocketId] && io.sockets.connected[socket.id]) {
+          // directly emit msg to the receiver
+          io.to(receiverSocketId).emit("audioMessage", message);
         } else {
-          console.log('not connected')
-          // query db with userId.
-          // sendPushNotification to the token from database.
+          console.log('receiver connection status', io.sockets.connected[receiverSocketId])
+          console.log('sender connection status', io.sockets.connected[socket.id])
+          // send push notification to receiver
           let id = data.to[0] && data.to[0].split("-")[1]
           const user = id && await usersCollection.findOne({ id: parseInt(id) });
-          console.log('11111111', user);
-          try {
-            user && sendPushNotification([user])
-          } catch (err) {
-            console.log('ERROR 1111', err)
-          }
+          user && sendPushNotification([user])
         }
       } else {
-        console.log('not found ....')
-        // query db with userId.
-        // sendPushNotification to the token from database.
+        // send push notification to receiver
         let id = data.to[0] && data.to[0].split("-")[1]
         const user = id && await usersCollection.findOne({ id: parseInt(id) });
         user && sendPushNotification([user])
       }
     } else {
-      console.log('data.group_name', data.group_name)
       let room = data.group_name;
-      console.log(data.to)
       const users = []
       await Promise.all(data.to.map(async element => {
         let receiverSocketId = findUserById(element);
@@ -172,22 +147,18 @@ io.on("connection", function (socket) {
           users.push(user)
         }
       }))
-      console.log('here', users);
       // sending push notification
-      sendPushNotification(users)
+      if (users.length) sendPushNotification(users)
       socket.broadcast.to(room).emit("audioMessage", message);
+      io.of('/').in(room).clients((error, socketIds) => {
+        if (error) console.log('err', err);
+        socketIds.forEach(socketId => io.sockets.sockets[socketId].leave(room));
+      });
     }
   });
 });
 
-// below if (receiverSocketId) {
-//socket.to(element).emit("audioMessage", message);
-// io.sockets.to(element).emit("audioMessage", message);
-// io.to(element).emit("audioMessage", message);
-//socket.broadcast.to(element).emit("audioMessage", message);
-
 function findUserById(name) {
-  console.log('name', name)
   for (socketId in people) {
     if (people[socketId].element === name) {
       return socketId;
@@ -197,11 +168,11 @@ function findUserById(name) {
 }
 
 //generate private room name for two users
-function getARoom(user1, user2) {
-  console.log('user1', user1)
-  console.log('user2', user2)
-  return 'privateRooom' + user1.element + "And" + user2.element;
-}
+// function getARoom(user1, user2) {
+//   console.log('user1', user1)
+//   console.log('user2', user2)
+//   return 'privateRooom' + user1.element + "And" + user2.element;
+// }
 
 http.listen(3001, function () {
   console.log("listening to port:3001");
